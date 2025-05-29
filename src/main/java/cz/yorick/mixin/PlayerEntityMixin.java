63b9aa172008d5907d93f125b78e985e5a88eb3d
Yorick_06 <unconfigured@null.spigotmc.org>
2025-05-29 12:50:34 +0200

    Players now cannot dismount when grabbed

---
+++ src/main/java/cz/yorick/mixin/PlayerEntityMixin.java
@@ -0,0 +1,22 @@
+package cz.yorick.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import cz.yorick.entity.DamnedOneEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @WrapMethod(method = "shouldDismount")
+    private boolean shouldDismount(Operation<Boolean> original) {
+        return original.call() && (!(this.getVehicle() instanceof DamnedOneEntity));
+    }
+}
